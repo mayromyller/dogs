0393632f19243f81a354d452c1fbba2cc0e090dd
mayro myller <deehmyller11@gmail.com>
2021-01-16 08:00:54 -0300

    create style for header component and adding image files

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 import style from './style.module.css'
+import { ReactComponent as Dogs } from '../../assets/dogs.svg'
 
 const Header = () => {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={style.header}>
+      <nav className={`${style.nav} container`}>
+        <Link
+          className={style.logo}
+          to="/"
+          aria-label="Dogs - desenho da carinha de um cachorro que é a logo do site"
+        >
+          <Dogs />
+        </Link>
+        <Link className={style.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   )
 }
 
